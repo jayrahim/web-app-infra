b00e22dc28d7edfa7a1e336d820331e54753f2ef
jayrahim <68208184+jayrahim@users.noreply.github.com>
2024-01-05 01:48:05 +0000

    Revert "Added data module to root level"
    This reverts commit 213a01235929f49abbc5e94be6e3179246a052c3.

--- main.tf
+++ main.tf
@@ -6,15 +6,3 @@ module "networking" {
   app_cidrs    = ["172.16.128.0/19", "172.16.160.0/19"]
   db_cidrs     = ["172.16.192.0/19", "172.16.224.0/19"]
 }
-
-module "data" {
-  source                  = "./modules/data"
-  allocated_storage       = 10
-  engine                  = "mysql"
-  engine_version          = "msyql8.0.5"
-  backup_retention_period = 1
-  db_subnet_ids           = module.networking.db_subnet_ids
-  major_engine_version    = "8.0"
-  vpc_id                  = module.networking.vpc_id
-  app_subnet_id           = module.networking.app_subnet_ids[0]
-}
